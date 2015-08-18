77273cb4ef00399ab95e7c3f3d9c969c4f1a0362
Yuce Tekol <yucetekol@gmail.com>
2015-08-18 03:06:42 +0300

    Initial commit

---
+++ tests/test_unittest.ts
@@ -0,0 +1,24 @@
+/// <reference path="../lib/unittest.ts" />
+
+module Unittest.Test {
+	export class UnittestTestCase extends Unittest.TestCase {
+		get name() {
+			return "UnittestTestCase";
+		}
+
+		test_assertEquivalent_number() {
+			this.assertEquivalent(1, 1);
+			this.assertNotEquivalent(1, 2);
+		}
+
+		test_assertEquivalent_array() {
+			let a1 = [1, 2, 3];
+			let a2 = [1, 2, 3];
+			let a3 = [5, 6, 7];
+			let a4 = [1, 2, 3, 4];
+			this.assertEquivalent(a1, a2);
+			this.assertNotEquivalent(a1, a3);
+			this.assertNotEquivalent(a1, a4);
+		}
+	}
+}
